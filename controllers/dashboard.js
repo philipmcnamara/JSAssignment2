c0ee37739e4f689f8b9bbacbab8feb86a8d065b6
Glitch (thread-bumpy-rifle) <none>
2020-07-19 13:13:01 +0000

    💇👶 Checkpoint ./views/partials/menu.hbs:17820309/20 ./views/about.hbs:17820309/121 ./controllers/about.js:17820309/28 ./controllers/dashboard.js:17820309/2878 ./views/partials/liststats:17820309/474 ./views/dashboard.hbs:17820309/253 ./views/partials/liststats.hbs:17820309/1603

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,14 +2,45 @@
 
 const logger = require("../utils/logger");
 
+const homers = {
+  name: 'Homer Simpson',
+  stats: [
+    {
+      weight: '200',
+      chest: '50',
+      thigh: '40',
+      upperArm: '30',
+      waist: '20',
+      hips: '10',
+    },
+    {
+      weight: '190',
+      chest: '50',
+      thigh: '40',
+      upperArm: '30',
+      waist: '20',
+      hips: '10',
+    },
+    {
+      weight: '180',
+      chest: '50',
+      thigh: '40',
+      upperArm: '30',
+      waist: '20',
+      hips: '10',
+    },
+  ],
+};
+
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
-      title: "Template 1 Dashboard",
+      title: 'Member 1 Dashboard',
+      member: homers
     };
     response.render("dashboard", viewData);
-  },
+  }
 };
 
 module.exports = dashboard;
