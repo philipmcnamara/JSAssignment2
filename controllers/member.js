f87f46e481bd9b17b5ab5355a1a0f96045443f90
Glitch (thread-bumpy-rifle) <none>
2020-07-19 14:44:52 +0000

    🎖🐞 Checkpoint ./routes.js:17820309/1030 ./controllers/member.js:17820309/348 ./models/member-store.js:17820309/878

--- controllers/member.js
+++ controllers/member.js
@@ -5,6 +5,8 @@ const memberCollection = require('../models/member-store.js');
 
 const member = {
   index(request, response) {
+    const memberId = request.params.id;
+    logger.info('Member id = ' + memberId);
     const viewData = {
       name: 'Member',
     };
--- routes.js
+++ routes.js
@@ -1,16 +1,16 @@
 "use strict";
 
-
 const express = require("express");
 const router = express.Router();
 
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
-const member = require('./controllers/member.js');
+const member = require("./controllers/member.js");
 
 router.get("/", dashboard.index);
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
-router.get('/member/id', member.index);
+router.get("/member/:id", member.index);
+//router.get("/member/:id/deletesong/:statid", member.deleteStat);
 
 module.exports = router;
