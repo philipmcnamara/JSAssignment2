994f5fca32361053e72d70dda916e0d420f6a5c6
Glitch (thread-bumpy-rifle) <none>
2020-07-21 12:31:11 +0000

    🗻🌠 Checkpoint ./controllers/accounts:17820309/1348 ./controllers/accounts.js:17820309/42 ./routes.js:17820309/1972 ./views/partials/menu.hbs:17820309/721

--- routes.js
+++ routes.js
@@ -1,19 +1,27 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const dashboard = require("./controllers/dashboard.js");
-const about = require("./controllers/about.js");
-const member = require("./controllers/member.js");
+const dashboard = require('./controllers/dashboard.js');
+const about = require('./controllers/about.js');
+const member = require('./controllers/member.js');
+const accounts = require('./controllers/accounts.js');
 
-router.get("/", dashboard.index);
-router.get("/dashboard", dashboard.index);
-router.get("/about", about.index);
-router.get("/member/:id", member.index);
-router.get("/member/:id/deletestat/:statid", member.deleteStat);
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
+
+router.get('/dashboard', dashboard.index);
 router.get('/dashboard/deletemember/:id', dashboard.deleteMember);
-router.post('/member/:id/addstat', member.addStat);
 router.post('/dashboard/addmember', dashboard.addMember);
+
+router.get('/about', about.index);
+router.get('/member/:id', member.index);
+router.get('/member/:id/deletestat/:statid', member.deleteStat);
+router.post('/member/:id/addstat', member.addStat);
 
 module.exports = router;
